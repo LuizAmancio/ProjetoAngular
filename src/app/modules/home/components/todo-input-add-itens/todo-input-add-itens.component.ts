a365d6575fd0abc1bb9b957f72f4aa591c8a4e23
LuizAmancioCeub <luiz.amancio@sempreceub.com>
2023-06-05 16:38:58 -0300

    Projeto Finalizado

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
   styleUrls: ['./todo-input-add-itens.component.css']
 })
-export class TodoInputAddItensComponent {
+export class TodoInputAddItensComponent implements OnInit{
+  //output recebe o evento emitido
+  @Output() public emitItemTaskList = new EventEmitter();
 
+  // recebe o input de adicionar nova task
+  public addItemTaskList:string = "";
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  // Pega evento emitido para jogar no todo-list
+  public submitItemTaskList(){
+
+    this.addItemTaskList = this.addItemTaskList.trim();  // remove os espaços a direita e a esquerda
+
+    if(this.addItemTaskList){ // so adiciona quando tiver algo no input
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList="";
+    }
+    
+  }
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 import { TaskList } from '../../model/task-list';
 
@@ -7,19 +7,20 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
-  // Array co os itens da Lista
-  public taskList: Array<TaskList> = [
-   {task: "tarefa 1", checked:true},
-   {task: "tarefa 2", checked:false}
-
-  ]
+  // Array com os itens da Lista
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]'); // passando a lista pra dentro do local Storage
 
   constructor(){}
 
-  ngOnInit(): void {
-    
+  ngDoCheck(): void {
+    this.setLocalStorate(); //Função para salvar lista 
+  }
+ 
+  //Seta o evento de addItem -> todo-input-add
+  public setEmitTaskList(event:string){ 
+    this.taskList.push({task:event, checked:false}); //adiciona na lista o evento captado no todo-input-add
   }
 
   // Deletar Item
@@ -33,9 +34,29 @@ export class TodoListComponent implements OnInit {
 
     if(confirm == true){
       this.taskList = [];
-    }
-    
+    }  
   }
-  
+
+  //Deletar quando item da lista for todo apagado
+  public validaInput(event:string, index:number){
+    if(!event.length){
+      const confirm = window.confirm("tarefa vazia, deseja deletar?");
+
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+
+  //Função para salvar lista no navegador
+  public setLocalStorate(){
+    if(this.taskList){
+      //Itens com checked no final da list
+    this.taskList.sort((first,last) => Number(first.checked) - Number(last.checked)); 
+
+    //Salvar lista no Local Storage
+    localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
+  }
 }
 
